97367ae32ed54fd9ce9b26d4fff24c90d36d2c59
stainer33 <spasang91@gmail.com>
2019-12-23 23:54:40 +0545

    Completed

--- app/src/main/java/com/e/employee_api/RegisterActivity.java
+++ app/src/main/java/com/e/employee_api/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.e.employee_api;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -31,32 +32,39 @@ public class RegisterActivity extends AppCompatActivity {
         etSalary=findViewById(R.id.etSalary);
         btnRegister=findViewById(R.id.btnRegister);
 
-        String name =etName.getText().toString();
-        Integer age = Integer.valueOf(etAge.getText().toString());
-        float salary = Float.parseFloat(etSalary.getText().toString());
-
-        EmployeeCUD employee = new EmployeeCUD(name,salary,age);
-
-        Retrofit retrofit = new Retrofit.Builder()
-                            .baseUrl(URL.base_url)
-                            .addConverterFactory(GsonConverterFactory.create())
-                            .build();
-
-        EmployeeAPI employeeAPI=retrofit.create(EmployeeAPI.class);
-        Call<Void>voidCall=employeeAPI.registerEmployee(employee);
-
-
-        voidCall.enqueue(new Callback<Void>() {
+        btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onResponse(Call<Void> call, Response<Void> response) {
-                Toast.makeText(RegisterActivity.this, "Register Successful", Toast.LENGTH_SHORT).show();
-            }
+            public void onClick(View v) {
+                String name =etName.getText().toString();
+                Integer age = Integer.valueOf(etAge.getText().toString());
+                float salary = Float.parseFloat(etSalary.getText().toString());
 
-            @Override
-            public void onFailure(Call<Void> call, Throwable t) {
-                Toast.makeText(RegisterActivity.this, "Error: "+ t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                EmployeeCUD employee = new EmployeeCUD(name,salary,age);
+
+                Retrofit retrofit = new Retrofit.Builder()
+                        .baseUrl(URL.base_url)
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
+
+                EmployeeAPI employeeAPI=retrofit.create(EmployeeAPI.class);
+                Call<Void>voidCall=employeeAPI.registerEmployee(employee);
+
+
+                voidCall.enqueue(new Callback<Void>() {
+                    @Override
+                    public void onResponse(Call<Void> call, Response<Void> response) {
+                        Toast.makeText(RegisterActivity.this, "Register Successful", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onFailure(Call<Void> call, Throwable t) {
+                        Toast.makeText(RegisterActivity.this, "Error: "+ t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
+
+
 
     }
 }
